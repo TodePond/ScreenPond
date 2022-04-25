e24e290c51e6345ebd89933e01aafcd2ff2c51c7
Lu[ke] Wilson <l2wilson94@gmail.com>
2022-04-25 15:10:30 +0000

    instruction in deno setup script

--- setup_deno_codespace.sh
+++ setup_deno_codespace.sh
@@ -1,3 +1,4 @@
+# in terminal, run: source setup_deno_codespace.sh
 curl -fsSL https://deno.land/install.sh | sh &&
 export PATH="/home/codespace/.deno/bin:$PATH" &&
 deno install -f --allow-net --allow-read https://deno.land/std@0.106.0/http/file_server.ts
